706ed05169fb19730b4c4073476e96af575f2ce2
LuisRocha9 <43184301+LuisRocha9@users.noreply.github.com>
2021-10-29 17:33:59 +0100

    Update queries.js

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,83 +1,78 @@
 export const getAllProjectByCategory = `
-query MyQuery($categoryName: [String]!) {
-    allWpProject(filter: {categories: {nodes: {elemMatch: {name: {in: $categoryName}}}}}) {
-        edges {
-            node {
-                id
-                title
-                slug
-                excerpt
-                featuredImage {
-                    node {
-                        sourceUrl
-                    }
-                }
-            }
+query MyQuery($categoryId: [ID]!) {
+  projects(where: {categoryIn: $categoryId}) {
+    edges {
+      node {
+        id
+        slug
+        title
+        excerpt
+        featuredImage {
+          node {
+            sourceUrl
+          }
         }
+      }
     }
+  }
 }`
 
 export const getAllProjectByTag = `
-query MyQuery($tagName: [String]!) {
-    allWpProject(filter: {tags: {nodes: {elemMatch: {name: {in: $tagName}}}}}) {
-        edges {
-            node {
-                id
-                title
-                slug
-                excerpt
-                featuredImage {
-                    node {
-                        sourceUrl
-                    }
-                }
-            }
+query MyQuery($tagIn: [ID]!) {
+  projects(where: {tagIn: $tagId}) {
+    edges {
+      node {
+        id
+        slug
+        title
+        excerpt
+        featuredImage {
+          node {
+            sourceUrl
+          }
         }
+      }
     }
+  }
 }`
 
 export const getAllProjectByCategoryTag = `
-query MyQuery($categoryName: [String]!, $tagName: [String]!) {
-    allWpProject(
-    filter: {
-        categories: {nodes: {elemMatch: {name: {in: $categoryName}}}},
-        tags: {nodes: {elemMatch: {name: {in: $tagName}}}}
-    }
-  ) {
-        edges {
-            node {
-                id
-                title
-                slug
-                excerpt
-                featuredImage {
-                    node {
-                        sourceUrl
-                    }
-                }
-            }
+query MyQuery($tagIn: [ID]!, $categoryId: [ID]!) {
+  projects(where: {categoryIn: $categoryId, tagIn: $tagId}) {
+    edges {
+      node {
+        id
+        slug
+        title
+        excerpt
+        featuredImage {
+          node {
+            sourceUrl
+          }
         }
+      }
     }
-}`
+  }
+}
+`
 
 export const getAllProjects = `
 {
-    allWpProject
-     {
-        edges {
-            node {
-                id
-                title
-                slug
-                excerpt
-                featuredImage {
-                    node {
-                        sourceUrl
-                    }
-                }
-            }
+  projects{
+    edges {
+      node {
+        id
+        slug
+        title
+        excerpt
+        featuredImage {
+          node {
+            sourceUrl
+          }
         }
+      }
     }
+  }
 }
 
 `
